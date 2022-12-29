95ba50a7f0dd09b02f03a27dad597a1356e616aa
TinySnow <lo.tinysnow.ol@outlook.com>
2022-12-29 15:20:31 +0800

    Updated onenote format shell

--- scripts/replace-onenote-list-style-to-normal-markdown-style.sh
+++ scripts/replace-onenote-list-style-to-normal-markdown-style.sh
@@ -3,16 +3,19 @@ set -Eeuxo pipefail
 sed -i -e "s/\-\-\-\-\-//g" $1
 sed -i -e "s/\-\-//g" $1
 
-sed -i -e "s/a.\ /- /g" $1
-sed -i -e "s/b.\ /- /g" $1
-sed -i -e "s/c.\ /- /g" $1
-sed -i -e "s/d.\ /- /g" $1
-sed -i -e "s/e.\ /- /g" $1
-sed -i -e "s/f.\ /- /g" $1
+sed -i -e "s/[a-z][\.+]\ /- /g" $1
+sed -i -e "s/[a-z][)+]\ /- /g" $1
+
+sed -i -e "s/[0-9][\.+]\ /- /g" $1
+sed -i -e "s/[0-9][)+]\ /- /g" $1
 
 sed -i -e "s/iv.\ /- /g" $1
+sed -i -e "s/v.\ /- /g" $1
+sed -i -e "s/vi.\ /- /g" $1
+sed -i -e "s/vii.\ /- /g" $1
 sed -i -e "s/iii.\ /- /g" $1
 sed -i -e "s/ii.\ /- /g" $1
 sed -i -e "s/i.\ /- /g" $1
 
+sed -i -e "s/\ \ \ \ \ \ \ /\ \ /g" $1
 sed -i -e "s/\ \ \ \ \ /\ \ /g" $1
